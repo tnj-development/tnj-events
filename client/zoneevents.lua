3901ba529088ce85d8b5e86fcfda854feddd97cf
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-04-18 19:39:16 +0930

    Zone Event

---
+++ client/zoneevents.lua
@@ -0,0 +1,18 @@
+local lastZone = nil
+CreateThread(function()
+	while true do
+        Wait(100) -- This doesnt have to be instant
+        local player = PlayerId()
+
+        if NetworkIsPlayerActive(player) then
+            local ped = PlayerPedId()
+            -- local coords = GetEntityCoords(ped)
+            local x,y,z = table.unpack(GetEntityCoords(ped, true))
+            currentZone = GetNameOfZone(x,y,z)
+            if currentZone ~= lastZone then
+                TriggerEvent("tnj-events:onZoneChange", currentZone, lastZone)
+                lastZone = currentZone
+            end
+        end
+    end
+end)
